712afff2145d446fc5b8cfe4ad73563ff0b52cb0
sandina <alejandro.sandin@roche.com>
2021-12-15 22:47:42 +0100

    juagando con @qualifier

--- src/main/java/com/sandin/learning/spring/basics/spring5steps/BinarySearchImpl.java
+++ src/main/java/com/sandin/learning/spring/basics/spring5steps/BinarySearchImpl.java
@@ -2,6 +2,7 @@ package com.sandin.learning.spring.basics.spring5steps;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -9,13 +10,14 @@ public class BinarySearchImpl {
 
 
     @Autowired
-    private SortAlgorithm bubbleSortAlgorithm;
+    @Qualifier("bubble")
+    private SortAlgorithm sortAlgorithm;
 
 
     public int binarySearch(int[] numbers, int numbersToSearchFor){
 
-        System.out.println("SortAlgorithmType:"+this.bubbleSortAlgorithm);
-        int[] sortedNumbers= bubbleSortAlgorithm.sort(numbers);
+        System.out.println("SortAlgorithmType:"+this.sortAlgorithm);
+        int[] sortedNumbers= sortAlgorithm.sort(numbers);
 
         return 3;
     }
--- src/main/java/com/sandin/learning/spring/basics/spring5steps/QuickSortAlgorithm.java
+++ src/main/java/com/sandin/learning/spring/basics/spring5steps/QuickSortAlgorithm.java
@@ -1,10 +1,12 @@
 package com.sandin.learning.spring.basics.spring5steps;
 
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
+@Qualifier("quick")
 public class QuickSortAlgorithm implements SortAlgorithm{
 
 
