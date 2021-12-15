3dfff0123324948eb1e84665a9b32c2e4109a89d
sandina <alejandro.sandin@roche.com>
2021-12-15 10:58:55 +0100

    adds the rest of files initial config repo

---
+++ src/main/java/com/sandin/learning/spring/basics/spring5steps/Spring5stepsApplication.java
@@ -0,0 +1,13 @@
+package com.sandin.learning.spring.basics.spring5steps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5stepsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5stepsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sandin/learning/spring/basics/spring5steps/Spring5stepsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sandin.learning.spring.basics.spring5steps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5stepsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
