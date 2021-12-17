239b10a219a642c508e5409146c6d37d379fa0a7
sandina <alejandro.sandin@roche.com>
2021-12-17 08:02:26 +0100

    pre y post construct callback al crear i eliminar beans.

--- src/main/java/com/sandin/learning/spring/basics/spring5steps/basic/BinarySearchImpl.java
+++ src/main/java/com/sandin/learning/spring/basics/spring5steps/basic/BinarySearchImpl.java
@@ -1,16 +1,23 @@
 package com.sandin.learning.spring.basics.spring5steps.basic;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 @Component
 @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class BinarySearchImpl {
 
+
+    public static Logger LOGGER = LoggerFactory.getLogger(BinarySearchImpl.class);
 
     @Autowired
     @Qualifier("bubble")
@@ -24,6 +31,17 @@ public class BinarySearchImpl {
 
         return 3;
     }
+
+    @PostConstruct
+    public void postConstruct(){
+        LOGGER.info("{}","PostConstruct:",this);
+    }
+
+    @PreDestroy
+    public void preDestroy(){
+        LOGGER.info("{}","PreDestroy:",this);
+    }
+
 
 }
 
