b7f45b102560ecf03ba33dc6acc3ef36666432f6
elDin-Dev <alexmaildese-dev@yahoo.com>
2021-12-29 00:26:55 +0100

    generar los beans usando el applicationContext.xml

---
+++ src/main/java/com/sandin/learning/spring/basics/spring5steps/xml/XmlJdbcConnection.java
@@ -0,0 +1,14 @@
+package com.sandin.learning.spring.basics.spring5steps.xml;
+
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE, proxyMode = ScopedProxyMode.TARGET_CLASS)
+public class XmlJdbcConnection {
+    public XmlJdbcConnection() {
+        System.out.println("XML JDBC Connection");
+    }
+}
