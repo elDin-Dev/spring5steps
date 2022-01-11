ba339369f6c665e000ea38c722b695f355b51fd6
sandina <alejandro.sandin@roche.com>
2022-01-11 12:34:45 +0100

    corriendo test con mockito sin contexto de spring

--- src/test/java/com/sandin/learning/spring/basics/spring5steps/cdi/SomeCDIBusinessTest.java
+++ src/test/java/com/sandin/learning/spring/basics/spring5steps/cdi/SomeCDIBusinessTest.java
@@ -5,27 +5,44 @@ import com.sandin.learning.spring.Appbasics.Spring5stepsBasicApplication;
 import com.sandin.learning.spring.basics.spring5steps.Spring5stepsCdiApplication;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.assertEquals;
 
-@RunWith(SpringRunner.class)
-@ContextConfiguration(classes = Spring5stepsCdiApplication.class)
+@RunWith(MockitoJUnitRunner.class)
 public class SomeCDIBusinessTest  {
 
 
 
-    @Autowired
+    @InjectMocks
     SomeCDIBusiness business;
+
+    @Mock
+    SomeCdiDao daoMock;
 
     @Test
     public void testBasicScenario(){
 
+        Mockito.when(daoMock.getData()).thenReturn(new int[]{2,4});
         int actualResult = business.findGreatest();
-        assertEquals(100, actualResult);
+        assertEquals(4, actualResult);
 
     }
+
+    @Test
+    public void testBasicScenario_no_elements(){
+
+        Mockito.when(daoMock.getData()).thenReturn(new int[]{});
+        int actualResult = business.findGreatest();
+        assertEquals(Integer.MIN_VALUE, actualResult);
+
+    }
+
 
 }
